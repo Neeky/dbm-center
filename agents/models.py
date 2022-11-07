4904e954315e8a36e6365de0f52ec6fc18fe2b0f
neekyjiang <neekyjiang@tencent.com>
2022-11-07 20:32:49 +0800

    🐞 fix: 确保 host 字段唯一

--- agents/models.py
+++ agents/models.py
@@ -7,16 +7,16 @@ class Agent(models.Model):
     用于保留系统里面每一个 Agent 的信息
     """
     # agent 所在主机的地址/域名
-    host = models.GenericIPAddressField()
+    host = models.GenericIPAddressField(unique=True)
 
     # agent 的版本号
     version = models.CharField(max_length=16)
 
-    # 注册到系统的时间
-    register_at = models.DateTimeField()
-
     # 端口
     port = models.IntegerField(default=8086)
+
+    # 注册到系统的时间
+    register_at = models.DateTimeField()
 
     # 最近一次上报心跳的时间点
     heartbeat_at = models.DateTimeField()

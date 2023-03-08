b6c361872a31f08e442ce7dd9eef16fdf163b0b4
neekyjiang <neekyjiang@tencent.com>
2023-03-08 13:53:54 +0800

    ✨feature: 调整默认 sqlite 数据库文件的位置

--- dbmcenter/settings.py
+++ dbmcenter/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'dbmcenter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbmcenter.sqlite3',
     }
 }
 
